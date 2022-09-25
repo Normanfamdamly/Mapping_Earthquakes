9159416a6255da0805909dc16fc753cd22272f1d
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-09-25 16:04:44 -0400

    updated

--- Mapping_Single_Points/Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/Mapping_Single_Points/js/logic.js
@@ -1,17 +1,15 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Add a marker to a map for Los Angles, California
-var marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Add a marker to a map 
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Add a marker to the map for Los Angels California
+L.circle([34.0522, -118.2437],{
+    radius: 100,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-});
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}');
